15dac6de301872c6f9bb186b71d1e2a132084b2e
NataliMi8 <92589644+NataliMi8@users.noreply.github.com>
2022-01-24 00:29:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ menu.onclick = function myFunction() {
     x.className="topnav";
   }
 }
+<script>
+var bigsize = "300"; // Размер большой картинки
+var smallsize = "150"; // Размер маленькой картинки
+function changeSizeImage(im) {
+if(im.height == bigsize) im.height = smallsize;
+else im.height = bigsize;
+}
+</script>
