940eafe52756d592754368e505ad4528ac185aa0
NataliMi8 <92589644+NataliMi8@users.noreply.github.com>
2022-01-24 01:06:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ menu.onclick = function myFunction() {
     x.className="topnav";
   }
 }
-<script>
-window.alert("Надеюсь, вам больше 18 лет?");
-window.alert("И у вас крепкие нервы?");
-window.alert("Иначе не надо нажимать на Ок");
-window.alert("Я вас честно предупреждал...");
-window.alert("Потом не жалуйтесь...");
-window.alert("Вам ещё не надоело?");
-document.write("<strong style='color:red'>ПРАВДА, КЛАССНАЯ СТРАНИЦА?</strong>")
-</script>
